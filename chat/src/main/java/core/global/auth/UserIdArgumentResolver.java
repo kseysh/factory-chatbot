604ddcfd91128066f39e956361a68d9c98eefecb
kseysh <kseysh123@naver.com>
2025-10-27 23:36:14 +0900

    refactor: 불필요한 어노테이션 제거

--- chat/src/main/java/core/global/auth/UserIdArgumentResolver.java
+++ chat/src/main/java/core/global/auth/UserIdArgumentResolver.java
@@ -1,12 +1,10 @@
 package core.global.auth;
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-@RequiredArgsConstructor
 public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
 
     private static final String CLIENT_ID_HEADER = "X-Client-Id";
