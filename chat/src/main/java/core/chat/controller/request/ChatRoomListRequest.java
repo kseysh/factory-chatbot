1513c90ca2be116209070d434a1553a82ba6458d
kseysh <kseysh123@naver.com>
2025-10-28 16:34:51 +0900

    refactor: Request가 생성자 실행시에 내부 검증을 거치도록 수정

--- chat/src/main/java/core/chat/controller/request/ChatRoomListRequest.java
+++ chat/src/main/java/core/chat/controller/request/ChatRoomListRequest.java
@@ -1,14 +1,23 @@
 package core.chat.controller.request;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import java.util.Optional;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class ChatRoomListRequest {
+    private static final int DEFAULT_CHAT_ROOMS_SIZE = 20;
+    private static final int MIN_CHAT_ROOM_PAGE_SIZE = 1;
+    private static final int MAX_CHAT_ROOM_PAGE_SIZE = 100;
 
-    private Long lastRoomId;
+    private final Optional<Long> lastRoomId;
 
-    private int size;
+    private final int size;
+
+    public ChatRoomListRequest(Optional<Long> lastRoomId, Integer size) {
+        this.lastRoomId = lastRoomId;
+        this.size = size == null ? DEFAULT_CHAT_ROOMS_SIZE : size;
+        if(this.size < MIN_CHAT_ROOM_PAGE_SIZE || this.size > MAX_CHAT_ROOM_PAGE_SIZE){
+            throw new IllegalArgumentException("size must be between 1 and 100");
+        }
+    }
 }
