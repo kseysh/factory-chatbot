7a0afa895eebb9d6e136c92b8e8a7fe04d892055
kseysh <kseysh123@naver.com>
2025-11-06 14:57:33 +0900

    fix: Assistance Message가 저장되도록 수정

--- mcp-client/src/main/java/core/mcpclient/service/LLMService.java
+++ mcp-client/src/main/java/core/mcpclient/service/LLMService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
@@ -22,17 +23,26 @@ public class LLMService {
         String conversationId = roomId.toString();
         chatMemory.add(conversationId, new UserMessage(question));
         Prompt prompt = new Prompt(chatMemory.get(conversationId));
-        return chatClient.prompt(prompt).call().content();
+
+        String response = chatClient.prompt(prompt)
+                .system(PromptContent.SYSTEM_PROMPT_DEFAULT_CHAT.getContent())
+                .call()
+                .content();
+        chatMemory.add(conversationId, new AssistantMessage(response));
+        return response;
     }
 
     public NewChatRoomInfo startNewChat(Long roomId, String question) {
         String conversationId = roomId.toString();
         chatMemory.add(conversationId, new UserMessage(question));
 
-        return chatClient.prompt(new Prompt(chatMemory.get(conversationId)))
+        NewChatRoomInfo response = chatClient.prompt(new Prompt(chatMemory.get(conversationId)))
                 .system(PromptContent.SYSTEM_PROMPT_CREATE_NEW_CHAT.getContent())
+                .system(PromptContent.SYSTEM_PROMPT_DEFAULT_CHAT.getContent())
                 .call()
                 .entity(NewChatRoomInfo.class);
+        chatMemory.add(conversationId, new AssistantMessage(response.answer()));
+        return response;
     }
 
 }
