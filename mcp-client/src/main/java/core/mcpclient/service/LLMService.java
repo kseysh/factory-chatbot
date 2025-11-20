0d9b27dae18456f80515369690a48b879ed54dcc
kseysh <kseysh123@naver.com>
2025-11-20 13:54:31 +0900

    fix: chatMemory가 UserMessage와 AssistantMessage를 모두 저장하도록 변

--- mcp-client/src/main/java/core/mcpclient/service/LLMService.java
+++ mcp-client/src/main/java/core/mcpclient/service/LLMService.java
@@ -5,11 +5,13 @@ import core.mcpclient.service.dto.NewChatRoomInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.ChatClient.CallResponseSpec;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -19,29 +21,29 @@ public class LLMService {
     private final ChatClient chatClient;
     private final ChatMemory chatMemory;
 
+    @Transactional
     public String chat(Long roomId, String question) {
         String conversationId = roomId.toString();
-        chatMemory.add(conversationId, new UserMessage(question));
-        Prompt prompt = new Prompt(chatMemory.get(conversationId));
+        CallResponseSpec response = chatClient.prompt(new Prompt(chatMemory.get(conversationId))).call();
+        String answer = response.content();
 
-        String response = chatClient.prompt(prompt)
-                .system(PromptContent.SYSTEM_PROMPT_DEFAULT_CHAT.getContent())
-                .call()
-                .content();
-        chatMemory.add(conversationId, new AssistantMessage(response));
-        return response;
+        chatMemory.add(conversationId, new UserMessage(question));
+        chatMemory.add(conversationId, new AssistantMessage(answer));
+        return answer;
     }
 
+    @Transactional
     public NewChatRoomInfo startNewChat(Long roomId, String question) {
         String conversationId = roomId.toString();
-        chatMemory.add(conversationId, new UserMessage(question));
 
-        NewChatRoomInfo response = chatClient.prompt(new Prompt(chatMemory.get(conversationId)))
+        CallResponseSpec response = chatClient.prompt(new Prompt(chatMemory.get(conversationId)))
                 .system(PromptContent.SYSTEM_PROMPT_CREATE_NEW_CHAT.getContent())
-                .call()
-                .entity(NewChatRoomInfo.class);
-        chatMemory.add(conversationId, new AssistantMessage(response.answer()));
-        return response;
+                .call();
+        NewChatRoomInfo answerDto = response.entity(NewChatRoomInfo.class);
+
+        chatMemory.add(conversationId, new UserMessage(question));
+        chatMemory.add(conversationId, new AssistantMessage(response.content()));
+        return answerDto;
     }
 
 }
